e72b402af99be62c26331822536d200227e7e761
Shubham Banerjee <shubham.banerjee@comviva.com>
2020-02-28 15:15:58 +0530

    Event binding

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Message from './components/Message'
 import Counter from './components/Counter'
 import FunctionClick from './components/FunctionClick'
 import ClassClick from './components/ClassClick'
+import EventBind from './components/EventBind'
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
       <FunctionClick/>
 
       <ClassClick/>
+
+      <EventBind/>
 
       {/* <Message /> */}
 
