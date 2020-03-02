0c5ecffc17eeccddc9a606c67c3019373e54a416
Shubham Banerjee <shubham.banerjee@comviva.com>
2020-03-02 09:02:15 +0530

    react1

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -8,6 +8,8 @@ class EventBind extends Component{
         this.state = {
             message: 'hello'
         }
+
+        this.clickHandler = this.clickHandler.bind(this)
     }
 
     clickHandler(){
@@ -22,8 +24,10 @@ class EventBind extends Component{
         return(
             <div>
                 <div>{this.state.message}</div>
-                <button onClick={this.clickHandler.bind(this)}>Click</button>
-            </div>
+                {/* <button onClick={this.clickHandler.bind(this)}>Click</button> */}
+                {/* <button onClick={()=> this.clickHandler()}>Click</button> */}
+                <button onClick={this.clickHandler}>Click</button>
+            </div> 
         )
     }
 
